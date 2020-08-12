d76879e230e4b94d51ac6902f5d4fc5e8212764d
admin <you@example.com>
2020-08-12 10:55:18 +0900

    add requests module name to requirement.txt

--- recipe_site/trial.py
+++
@@ -1,9 +0,0 @@
-import requests
-import json
-
-api_key = 1044473351726139211
-r = requests.get('https://app.rakuten.co.jp/services/api/Recipe/CategoryRanking/20170426?applicationId={}&categoryId=10&elements=recipeTitle,foodImageUrl,rank'.format(api_key))
-data = r.json()
-#print(json.dumps(data, indent=4))
-data = json.dumps(data['result'], indent=2, ensure_ascii=False)
-print(json.loads(data))
